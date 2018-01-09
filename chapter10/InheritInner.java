aa3b87747913d29b62dda0f21072e22a055ea839
zhanghui <huizhang1995@gmail.com>
2018-01-09 19:19:10 +0800

    chapter10:over

---
+++ chapter10/InheritInner.java
@@ -0,0 +1,18 @@
+package chapter10;
+
+class WithInner {
+	class Inner {
+
+	}
+}
+
+public class InheritInner extends WithInner.Inner {
+	public InheritInner(WithInner wi) {
+		wi.super();
+	}
+
+	public static void main() {
+		WithInner wi = new WithInner();
+		InheritInner ii = new InheritInner(wi);
+	}
+}
