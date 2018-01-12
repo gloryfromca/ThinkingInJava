5cbaa167ca7349bae3b6fe9081006332e8e0545e
zhanghui <huizhang1995@gmail.com>
2018-01-12 19:28:42 +0800

    chapter13:ongoing

---
+++ chapter13/InfiniteRecursion.java
@@ -0,0 +1,15 @@
+package chapter13;
+
+public class InfiniteRecursion {
+	@Override
+	public String toString() {
+		// `+ this` will call toString() that is what we're writing.
+		// return "memory address: " + this;
+		return super.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new InfiniteRecursion());
+	}
+
+}
