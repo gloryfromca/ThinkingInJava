4dceccbf29d935c328ccdf246a6cdd7c0896db77
gloryfromca <huizhang1995@gmail.com>
2018-01-15 00:08:09 +0800

    chapter14:ongoing

---
+++ chapter13/UsageOfScanner.java
@@ -0,0 +1,24 @@
+package chapter13;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.Scanner;
+
+
+public class UsageOfScanner {
+	static BufferedReader input = new BufferedReader(new StringReader("zhanghui"+
+		"\n 22 15.2"));
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(input);
+		String name = scanner.nextLine();
+		int age = scanner.nextInt();
+		double favorite = scanner.nextDouble();
+		System.out.println("name: "+name);
+		System.out.println("age: "+ age);
+		System.out.println("favorite double: "+ favorite);
+		
+		
+		
+	}
+
+}
