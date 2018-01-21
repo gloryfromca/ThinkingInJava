c691a68c1e8603e316490d8557462b526cfb2209
gloryfromca <huizhang1995@gmail.com>
2018-01-21 23:40:16 +0800

    chapter17:ongoing

---
+++ chapter16/ArrayAutoboxing.java
@@ -0,0 +1,20 @@
+package chapter16;
+
+public class ArrayAutoboxing {
+
+	public static void main(String[] args) {
+		Integer[] integers = { 1, 2, 2, 4, 3 };
+		int[] ints1 = { 1, 2, 2, 4, 3 };
+		int[] ints2 = { new Integer(1), 2, 2, 4, 3 };
+		if (ints2[0] == 1) {
+			System.out.println("Integer 1 == int 1");
+		}
+		ints2 = ints1;
+
+		// can't convert int[] to Integer[]
+		// integers = ints2;
+		// ints2 = integers;
+
+	}
+
+}
