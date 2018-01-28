389e2bc6cea03eb76c36c1a028090959225b480f
gloryfromca <huizhang1995@gmail.com>
2018-01-28 23:48:48 +0800

    chapter18:ongoing

---
+++ chapter18/AvailableCharSets.java
@@ -0,0 +1,26 @@
+package chapter18;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.SortedMap;
+
+public class AvailableCharSets {
+
+	public static void main(String[] args) {
+		SortedMap<String, Charset> charsets = Charset.availableCharsets();
+		Iterator<String> it = charsets.keySet().iterator();
+		String cs;
+		while (it.hasNext()) {
+			cs = it.next();
+			System.out.println(cs + ":");
+			Iterator<String> itcs = Charset.forName(cs).aliases().iterator();
+			while (itcs.hasNext()) {
+				System.out.print(itcs.next() + ", ");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
---
+++ chapter18/FilesForTest.java
@@ -0,0 +1,10 @@
+package chapter18;
+
+public class FilesForTest {
+	static String inMac = "/Users/zhanghui/testFileFolder/s.in";
+	static String outMac = "/Users/zhanghui/testFileFolder/s.out";
+	static String zipMac = "/Users/zhanghui/testFileFolder/s.zip";
+	static String s1Mac = "/Users/zhanghui/testFileFolder/s.1";
+	static String s2Mac = "/Users/zhanghui/testFileFolder/s.2";
+
+}
